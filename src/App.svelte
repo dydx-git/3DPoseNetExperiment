1ad1c5330c42354d50128859be629760f7dbc423
Muhammad Taha <33485877+dydx-git@users.noreply.github.com>
2021-05-16 18:53:31 -0700

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script lang='ts'>
+  let count: number = 0;
+
+  function increment () { count++ }
+  function decrement () { count-- }
+</script>
+
+<button on:click={decrement}>-</button>
+<span>{count}</span>
+<button on:click={increment}>+</button>
+
+<style>
+	span {
+		color: red;
+		font-weight: bold;
+	}
+</style>
